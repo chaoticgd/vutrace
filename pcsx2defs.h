0257c2aad7440e950d607e33149d6d5c7b8708b1
chaoticgd <chaoticgd@protonmail.com>
2020-04-25 22:54:24 +0100

    Initial commit

---
+++ pcsx2defs.h
@@ -0,0 +1,146 @@
+/*  PCSX2 - PS2 Emulator for PCs
+ *  Copyright (C) 2002-2010  PCSX2 Dev Team
+ *  Copyright (C) 2020 chaoticgd
+ *
+ *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesser General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  PCSX2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with PCSX2.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+
+static const uint VU1_MEMSIZE	= 0x4000;		// 16kb
+static const uint VU1_PROGSIZE	= 0x4000;		// 16kb
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef int32_t s32;
+typedef uint32_t u32;
+typedef int16_t s16;
+typedef uint16_t u16;
+typedef int8_t s8;
+typedef uint8_t u8;
+typedef unsigned int uint;
+
+union VECTOR {
+	struct {
+		float x,y,z,w;
+	} f;
+	struct {
+		u32 x,y,z,w;
+	} i;
+
+	float F[4];
+
+	u64 UD[2];      //128 bits
+	s64 SD[2];
+	u32 UL[4];
+	s32 SL[4];
+	u16 US[8];
+	s16 SS[8];
+	u8  UC[16];
+	s8  SC[16];
+};
+
+struct REG_VI {
+	union {
+		float F;
+		s32   SL;
+		u32	  UL;
+		s16   SS[2];
+		u16   US[2];
+		s8    SC[4];
+		u8    UC[4];
+	};
+	u32 padding[3]; // needs padding to make them 128bit; VU0 maps VU1's VI regs as 128bits to addr 0x4xx0 in
+					// VU0 mem, with only lower 16 bits valid, and the upper 112bits are hardwired to 0 (cottonvibes)
+};
+
+struct fdivPipe {
+	int enable;
+	REG_VI reg;
+	u32 sCycle;
+	u32 Cycle;
+	u32 statusflag;
+};
+
+struct efuPipe {
+	int enable;
+	REG_VI reg;
+	u32 sCycle;
+	u32 Cycle;
+};
+
+struct fmacPipe {
+	int enable;
+	int reg;
+	int xyzw;
+	u32 sCycle;
+	u32 Cycle;
+	u32 macflag;
+	u32 statusflag;
+	u32 clipflag;
+};
+
+struct ialuPipe {
+	int enable;
+	int reg;
+	u32 sCycle;
+	u32 Cycle;
+};
+
+struct VURegs
+{
+	VECTOR	VF[32]; // VF and VI need to be first in this struct for proper mapping
+	REG_VI	VI[32]; // needs to be 128bit x 32 (cottonvibes)
+
+	VECTOR ACC;
+	REG_VI q;
+	REG_VI p;
+
+	uint idx;		// VU index (0 or 1)
+
+	// flags/cycle are needed by VIF dma code, so they have to be here (for now)
+	// We may replace these by accessors in the future, if merited.
+	u32 cycle;
+	u32 flags;
+
+	// Current opcode being interpreted or recompiled (this var is used by Interps and superVU
+	// but not microVU.  Would like to have it local to their respective classes... someday)
+	u32 code;
+
+	// branch/branchpc are used by interpreter only, but making them local to the interpreter
+	// classes requires considerable code refactoring.  Maybe later. >_<
+	u32 branch;
+	u32 branchpc;
+	u32 delaybranchpc;
+	bool takedelaybranch;
+
+	// MAC/Status flags -- these are used by interpreters and superVU, but are kind of hacky
+	// and shouldn't be relied on for any useful/valid info.  Would like to move them out of
+	// this struct eventually.
+	u32 macflag;
+	u32 statusflag;
+	u32 clipflag;
+
+	u8 *Mem;
+	u8 *Micro;
+
+	u32 ebit;
+
+	u8 VIBackupCycles;
+	u32 VIOldValue;
+	u32 VIRegNumber;
+
+	fmacPipe fmac[8];
+	fdivPipe fdiv;
+	efuPipe efu;
+	ialuPipe ialu[8];
+};
---
+++ vutrace.cpp
@@ -0,0 +1,286 @@
+/*
+	vutrace - Hacky VU tracer/debugger.
+	Copyright (C) 2020 chaoticgd
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
+
+#include "pcsx2defs.h"
+
+struct Snapshot
+{
+	VURegs registers;
+	u8 memory[VU1_MEMSIZE];
+	u8 program[VU1_PROGSIZE];
+	int disassembly;
+};
+
+struct AppState
+{
+	std::size_t current_snapshot = 0;
+	std::vector<Snapshot> snapshots;
+	std::vector<std::vector<std::string>> disassemblies;
+};
+
+void update_gui(AppState &app);
+void snapshots_window(AppState &app);
+void registers_window(AppState &app);
+void memory_window(AppState &app);
+void disassembly_window(AppState &app);
+std::vector<Snapshot> parse_trace(AppState &app, std::string dir_path);
+void init_gui(GLFWwindow **window);
+
+int main(int argc, char **argv)
+{
+	if(argc != 2) {
+		fprintf(stderr, "You must specify a trace directory.\n");
+		return 1;
+	}
+	
+	GLFWwindow *window;
+	int width, height;
+	init_gui(&window);
+	
+	AppState app;
+	app.snapshots = parse_trace(app, argv[1]);
+	
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		update_gui(app);
+
+		ImGui::Render();
+		glfwMakeContextCurrent(window);
+		glfwGetFramebufferSize(window, &width, &height);
+
+		glViewport(0, 0, width, height);
+		glClearColor(0, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwMakeContextCurrent(window);
+		glfwSwapBuffers(window);
+	}
+		
+	glfwDestroyWindow(window);
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	glfwTerminate();
+}
+
+void update_gui(AppState &app)
+{
+	if(ImGui::Begin("Snapshots"))   snapshots_window(app);   ImGui::End();
+	if(ImGui::Begin("Registers"))   registers_window(app);   ImGui::End();
+	if(ImGui::Begin("Memory"))      memory_window(app);      ImGui::End();
+	if(ImGui::Begin("Disassembly")) disassembly_window(app); ImGui::End();
+}
+
+void snapshots_window(AppState &app)
+{
+	ImVec2 size = ImGui::GetWindowSize();
+	size.x -= 16;
+	size.y -= 64;
+	ImGui::PushItemWidth(-1);
+	if(ImGui::ListBoxHeader("##snapshots", size)) {
+		for(std::size_t i = 0; i < app.snapshots.size(); i++) {
+			Snapshot& snap = app.snapshots[i];
+			std::string str = std::to_string(i);
+			if(ImGui::Selectable(str.c_str(), i == app.current_snapshot)) {
+				app.current_snapshot = i;
+			}
+		}
+		ImGui::ListBoxFooter();
+	}
+	ImGui::PopItemWidth();
+}
+
+void registers_window(AppState &app)
+{
+	Snapshot &current = app.snapshots[app.current_snapshot];
+	VURegs &regs = current.registers;
+	
+	auto draw_vec_reg = [](const char *name, VECTOR value) {
+		ImGui::Text("%s = %.4f %.4f %.4f %.4f",
+			name, value.F[0], value.F[1], value.F[2], value.F[3]);
+	};
+	
+	for(int i = 0; i < 32; i++) {
+		std::string name = std::string("vf") + std::to_string(i);
+		draw_vec_reg(name.c_str(), regs.VF[i]);
+	}
+}
+
+void memory_window(AppState &app)
+{
+	Snapshot &current = app.snapshots[app.current_snapshot];
+	
+	static const int ROW_SIZE = 32;
+	
+	if(ImGui::BeginChild("##rows")) {
+		for(int i = 0; i < VU1_MEMSIZE / ROW_SIZE; i++) {
+			u8 *data = current.memory + i * ROW_SIZE;
+			std::stringstream line;
+			for(int j = 0; j < ROW_SIZE; j++) {
+				int val = data[j] & 0xff;
+				if(val < 0x10) line << "0";
+				line << std::hex << val << " ";
+			}
+			ImGui::Text("%s", line.str().c_str());
+		}
+		ImGui::EndChild();
+	}
+}
+
+void disassembly_window(AppState &app)
+{
+	Snapshot &current = app.snapshots[app.current_snapshot];
+	std::vector<std::string> &listing = app.disassemblies[current.disassembly];
+	for(std::string &line : listing) {
+		ImGui::Text("%s", line.c_str());
+	}
+}
+
+std::vector<Snapshot> parse_trace(AppState &app, std::string dir_path)
+{
+	std::vector<Snapshot> snapshots;
+	
+	std::string vu1_file = dir_path + "/vu1.bin";
+	
+	FILE *trace = fopen(vu1_file.c_str(), "rb");
+	if(trace == nullptr) {
+		fprintf(stderr, "Error: Failed to read trace!\n");
+		exit(1);
+	}
+	
+	// My VURegs varies in size to their one.
+	// Instead of fixing it, here's this horrible thing.
+	static const int hack_size = sizeof(VURegs) - 0x10;
+	
+	static const int reg_pos = 0x10;
+	static const int mem_pos = reg_pos + hack_size + 0x10;
+	static const int prog_pos = mem_pos + VU1_MEMSIZE + 0x10;
+		
+	char buffer[prog_pos + VU1_PROGSIZE];
+	while(fread(buffer, sizeof(buffer), 1, trace) == 1) {
+		printf("Loading %ld\n", ftell(trace));
+		
+		if(memcmp(buffer, "REGISTERS=======", 0x10) != 0) {
+			fprintf(stderr, "Error: REGISTERS signature missing!");
+			exit(1);
+		}
+		
+		Snapshot current;
+		memcpy(&current.registers, buffer + reg_pos, hack_size);
+		//memset(&current.registers + hack_size, 0, 0x10);
+		memcpy(current.memory, buffer + mem_pos, VU1_MEMSIZE);
+		memcpy(current.program, buffer + prog_pos, VU1_PROGSIZE);
+		std::vector<std::string> listing;
+		fseek(trace, 0x10, SEEK_CUR); // Skip "DISASSEMBLY====="
+		while(1) {
+			if(feof(trace)) {
+				break;
+			}
+			
+			char signature[0x10];
+			fread(signature, 0x10, 1, trace);
+			fseek(trace, -0x10, SEEK_CUR);
+			if(memcmp(signature, "REGISTERS=======", 0x10) == 0) {
+				break;
+			}
+			
+			char disasm[256];
+			memset(disasm, '\0', 256);
+			int i;
+			for(i = 0; i < 255; i++) {
+				char next = fgetc(trace);
+				if(feof(trace) || next == '\0') {
+					break;
+				} else {
+					disasm[i] = next;
+				}
+			}
+			if(i != 255) {
+				while(!feof(trace) && fgetc(trace) != '\0');
+			}
+			listing.push_back(disasm);
+		}
+		
+		if(app.disassemblies.size() < 1 ||
+				!std::equal(listing.begin(), listing.end(), app.disassemblies.back().begin())) {
+			app.disassemblies.push_back(listing);
+		}
+		current.disassembly = app.disassemblies.size() - 1;
+		snapshots.push_back(current);
+	}
+	if(!feof(trace)) {
+		fprintf(stderr, "Error: Failed to read trace!\n");
+		exit(1);
+	}
+	
+	return snapshots;
+}
+
+void init_gui(GLFWwindow **window)
+{
+	if(!glfwInit()) {
+		fprintf(stderr, "Cannot load GLFW.");
+		exit(1);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+	*window = glfwCreateWindow(1280, 720, "vutrace", NULL, NULL);
+	if(*window == nullptr) {
+		fprintf(stderr, "Cannot create GLFW window.");
+		exit(1);
+	}
+
+	glfwMakeContextCurrent(*window);
+	glfwSwapInterval(1); // vsync
+
+	if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		fprintf(stderr, "Cannot load GLAD.");
+		exit(1);
+	}
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigDockingWithShift = true;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(*window, true);
+	ImGui_ImplOpenGL3_Init("#version 120");
+}

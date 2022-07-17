cd9022c4fa048f9dea19e01115345654e6880d50
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2022-07-17 08:05:21 +0100

    Update VURegs struct to work with the new 64 bit builds of pcsx2

--- pcsx2defs.h
+++ pcsx2defs.h
@@ -33,41 +33,62 @@ static const uint VU1_MEMSIZE	= 0x4000;		// 16kb
 static const uint VU1_PROGSIZE	= 0x4000;		// 16kb
 static const uint TPC = 26;
 
-union VECTOR {
-	struct {
-		float x,y,z,w;
+struct u128 {
+	u64 low;
+	u64 high;
+};
+
+struct s128 {
+	u64 low;
+	u64 high;
+};
+
+union VECTOR
+{
+	struct
+	{
+		float x, y, z, w;
 	} f;
-	struct {
-		u32 x,y,z,w;
+	struct
+	{
+		u32 x, y, z, w;
 	} i;
 
 	float F[4];
 
-	u64 UD[2];      //128 bits
+	u128 UQ;
+	s128 SQ;
+	u64 UD[2]; //128 bits
 	s64 SD[2];
 	u32 UL[4];
 	s32 SL[4];
 	u16 US[8];
 	s16 SS[8];
-	u8  UC[16];
-	s8  SC[16];
+	u8 UC[16];
+	s8 SC[16];
 };
 
-struct REG_VI {
-	union {
+struct REG_VI
+{
+	union
+	{
 		float F;
-		s32   SL;
-		u32	  UL;
-		s16   SS[2];
-		u16   US[2];
-		s8    SC[4];
-		u8    UC[4];
+		s32 SL;
+		u32 UL;
+		s16 SS[2];
+		u16 US[2];
+		s8 SC[4];
+		u8 UC[4];
 	};
 	u32 padding[3]; // needs padding to make them 128bit; VU0 maps VU1's VI regs as 128bits to addr 0x4xx0 in
-					// VU0 mem, with only lower 16 bits valid, and the upper 112bits are hardwired to 0 (cottonvibes)
+		// VU0 mem, with only lower 16 bits valid, and the upper 112bits are hardwired to 0 (cottonvibes)
 };
 
-struct fdivPipe {
+//#define VUFLAG_BREAKONMFLAG		0x00000001
+#define VUFLAG_MFLAGSET 0x00000002
+#define VUFLAG_INTCINTERRUPT 0x00000004
+struct fdivPipe
+{
 	int enable;
 	REG_VI reg;
 	u32 sCycle;
@@ -75,17 +96,21 @@ struct fdivPipe {
 	u32 statusflag;
 };
 
-struct efuPipe {
+struct efuPipe
+{
 	int enable;
 	REG_VI reg;
 	u32 sCycle;
 	u32 Cycle;
 };
 
-struct fmacPipe {
-	int enable;
-	int reg;
-	int xyzw;
+struct fmacPipe
+{
+	u32 regupper;
+	u32 reglower;
+	int flagreg;
+	u32 xyzwupper;
+	u32 xyzwlower;
 	u32 sCycle;
 	u32 Cycle;
 	u32 macflag;
@@ -93,8 +118,8 @@ struct fmacPipe {
 	u32 clipflag;
 };
 
-struct ialuPipe {
-	int enable;
+struct ialuPipe
+{
 	int reg;
 	u32 sCycle;
 	u32 Cycle;
@@ -102,23 +127,24 @@ struct ialuPipe {
 
 struct VURegs
 {
-	VECTOR	VF[32]; // VF and VI need to be first in this struct for proper mapping
-	REG_VI	VI[32]; // needs to be 128bit x 32 (cottonvibes)
+	VECTOR VF[32]; // VF and VI need to be first in this struct for proper mapping
+	REG_VI VI[32]; // needs to be 128bit x 32 (cottonvibes)
 
 	VECTOR ACC;
 	REG_VI q;
 	REG_VI p;
 
-	uint idx;		// VU index (0 or 1)
+	uint idx; // VU index (0 or 1)
 
 	// flags/cycle are needed by VIF dma code, so they have to be here (for now)
 	// We may replace these by accessors in the future, if merited.
 	u32 cycle;
 	u32 flags;
 
-	// Current opcode being interpreted or recompiled (this var is used by Interps and superVU
+	// Current opcode being interpreted or recompiled (this var is used by Interps
 	// but not microVU.  Would like to have it local to their respective classes... someday)
 	u32 code;
+	u32 start_pc;
 
 	// branch/branchpc are used by interpreter only, but making them local to the interpreter
 	// classes requires considerable code refactoring.  Maybe later. >_<
@@ -126,27 +152,48 @@ struct VURegs
 	u32 branchpc;
 	u32 delaybranchpc;
 	bool takedelaybranch;
+	u32 ebit;
+	u32 pending_q;
+	u32 pending_p;
+	u32 blockhasmbit;
 
-	// MAC/Status flags -- these are used by interpreters and superVU, but are kind of hacky
+	alignas(16) u32 micro_macflags[4];
+	alignas(16) u32 micro_clipflags[4];
+	alignas(16) u32 micro_statusflags[4];
+	// MAC/Status flags -- these are used by interpreters but are kind of hacky
 	// and shouldn't be relied on for any useful/valid info.  Would like to move them out of
 	// this struct eventually.
 	u32 macflag;
 	u32 statusflag;
 	u32 clipflag;
 
-	u32 Mem;
-	u32 Micro;
+	s32 nextBlockCycles;
 
-	u32 ebit;
+	u8* Mem;
+	u8* Micro;
+
+	u32 xgkickaddr;
+	u32 xgkickdiff;
+	u32 xgkicksizeremaining;
+	u32 xgkicklastcycle;
+	u32 xgkickcyclecount;
+	u32 xgkickenable;
+	u32 xgkickendpacket;
 
 	u8 VIBackupCycles;
 	u32 VIOldValue;
 	u32 VIRegNumber;
 
-	fmacPipe fmac[8];
+	fmacPipe fmac[4];
+	u32 fmacreadpos;
+	u32 fmacwritepos;
+	u32 fmaccount;
 	fdivPipe fdiv;
 	efuPipe efu;
-	ialuPipe ialu[8];
+	ialuPipe ialu[4];
+	u32 ialureadpos;
+	u32 ialuwritepos;
+	u32 ialucount;
 };
 
 #endif
